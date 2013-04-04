9ba2e9c19e343833007f0f36f0df177d088b290c
alangh.uabitapevi.si4@gmail.com <alangh.uabitapevi.si4@gmail.com@20277c60-b7fb-e432-ca00-9afdf70059ae>
2013-04-04 11:37:49 +0000

    Commit inicial: o jogo contém um conjunto de procedimentos básicos para a criação e impressão de números aleatórios e para a contagem do tempo através de recursos sonoros.

---
+++ trunk/memorise/random.asm
@@ -0,0 +1,37 @@
+## Contém procedimentos para a geração dos números randômicos utilizados no jogo.
+
+# Procedimento que cria uma seed para o algorítmo de geração de números randômicos a partir da data atual do sistema.
+# Estabelece-se que o id do gerador de números pseudo randômicos será 1.
+# Esse id deverá ser utilizado pela syscall que gerará os números randômicos propriamente ditos.
+.macro set_seed
+	# carrega o time service.
+	li $v0, 30
+	# A syscall gerará um valor de 64 bits que corresponde a data atual do sistema em milisegundos.  
+	syscall
+
+	# Move os 32 bits menos significativos do tempo para o registrador $a1 (o segundo argumento para a syscall posterior). 
+	move $a1, $a0
+	# Carrega a syscall set seed.
+	li $v0, 40
+	# Define o id para o gerador de números randômicos.
+	li $a0, 1
+			syscall
+
+	.end_macro
+
+# Procedimento que gera números aleatórios e os armazena na memória (no label 			# random_numbers).
+.macro generate_random_numbers($target, $length, $max)
+	la $t0, $target # ponteiro para o array na memória.
+	lw $t1, $length # contador para o loop.
+
+	loop_random:
+		# id do gerador de números randômicos definido na subrotina set_seed.
+		li $a0, 1 
+		lw $a1, $max # o valor limite para o intervalo.  
+		li $v0, 42 # carrega o random int range service.
+		syscall
+		sw $a0, 0 ($t0) # armazena o número aleatório na memória.
+		sub $t1, $t1, 1 # decrementa o contador.
+		addi $t0, $t0, 4 # incrementa o "ponteiro".
+		bne $t1, $0, loop_random
+.end_macro
---
+++ trunk/memorise/sound_efects.asm
@@ -0,0 +1,26 @@
+## Contém procedimentos relacionados a geração de sons para o jogo.
+ 
+# Procedimento que emite um som de contagem a cada segundo, indicando a passagem do tempo e interrompendo a thread atual.
+# O procedimento será utilizado para marcar o tempo em que os números ficarão visíveis na tela.
+# Após o fim do tempo determinado, um som é emitido indicando que o tempo para o jogador memorizar os números acabou.    
+.macro clock($time)
+	lw $t0, $time
+	# Carrega o MIDI out synchronous service
+	li $v0, 33
+	li $a0, 61 # pitch
+	li $a1, 900 # duração em milisegundos
+	li $a2, 12 # percussão cromática 
+	li $a3, 127 # volume máximo
+
+	loop_clock:
+		sub $t0, $t0, 1
+		syscall
+		bne $t0, $0, loop_clock
+
+	# A seguir, notificamos que o tempo acabou através de um aviso sonoro
+	li $a0, 55 # pitch
+	li $a1, 1000 # duração em milisegundos
+	li $a2, 120 # efeito sonoro 
+
+	syscall
+	.end_macro
