c65a70e26c5a2b22055f312730e1dc5e390b9c6d
altamir.uabitapevi.si4 <altamir.uabitapevi.si4@gmail.com>
2013-04-06 13:52:11 +0000

    Foi adicionado um template .asm para geração de números pseudo-randômicos.

--- interleaved/randomiza.asm
+++ interleaved/randomiza.asm
@@ -4,7 +4,7 @@
 # I representa uma constante de incremento
 # M representa um módulo constante
 # Esta função assume que os argumentos estão em $a0, $a1, $a2 e $a3 e todos são
-# números inteiros positivos (portanto não seta o bit de estouro ou overflow)
+# números inteiros positivos (portanto não seta o bit de estouro ou overflow) 
 
 mulu $r2, $a0, $a1                                    # Z * U
 addu $r2, $a2, $r2                                    # Z * U + I
