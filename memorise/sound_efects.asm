149c1b3396e8dc234f5e21066c60773be37fb437
alangh.uabitapevi.si4 <alangh.uabitapevi.si4@gmail.com>
2013-04-16 01:36:45 +0000

    Adicionando novos efeitos sonoros a sound_efects.asm. Corrigido um erro na finalização do jogo quando o jogador vencia o último nível. Com esse commit a primeira versão do jogo está liberada.

--- memorise/sound_efects.asm
+++ memorise/sound_efects.asm
@@ -29,11 +29,11 @@
 	.macro welcome
 	# Carrega o MIDI out synchronous service
 	li $v0, 33
-li $a0, 60 # pitch
-li $a1, 900 # duração em milisegundos
-li $a2, 24 # guitarra
-li $a3, 127 # volume máximo
-li $t0, 4 # contador para o loop
+	li $a0, 60 # pitch
+	li $a1, 900 # duração em milisegundos
+	li $a2, 24 # guitarra
+	li $a3, 127 # volume máximo
+	li $t0, 4 # contador para o loop
 
 	loop_welcome:
 		sub $t0, $t0, 1
@@ -42,3 +42,55 @@ li $t0, 4 # contador para o loop
 		sub $a1, $a1, 100
 		bne $t0, $0, loop_welcome
 	.end_macro
+
+	# Dispara um efeito sonoro indicando que o jogador errou sua tentativa.
+	.macro error
+	# Carrega o MIDI out synchronous service
+	li $v0, 33
+	li $a0, 65 # pitch
+	li $a1, 1000 # duração em milisegundos
+	li $a2, 56 
+	li $a3, 127 # volume máximo
+	li $t0, 3 # contador para o loop
+
+	loop_error:
+		sub $t0, $t0, 1
+		syscall
+		sub $a0, $a0, 2
+		bne $t0, $0, loop_error 
+	.end_macro
+
+	# Cria um efeito sonoro a ser utilizado para indicar o acerto do jogador. 
+	.macro hit
+		# Carrega o MIDI out synchronous service
+	li $v0, 33
+	li $a0, 60 # pitch
+	li $a1, 1000 # duração em milisegundos
+	li $a2, 96 # efeitos sintetizados 
+	li $a3, 127 # volume máximo
+	li $t0, 3 # contador para o loop
+
+	loop_hit:
+		sub $t0, $t0, 1
+		syscall
+		addi $a0, $a0, 2
+		sub $a1, $a1, 100
+		bne $t0, $0, loop_hit
+.end_macro
+
+.macro victory
+	# Carrega o MIDI out synchronous service
+	li $v0, 33
+	li $a0, 30 # pitch
+	li $a1, 200 # duração em milisegundos
+	li $a2, 0 # piano
+	li $a3, 127 # volume máximo
+	li $t0, 7 # contador para o loop
+
+	loop_victory:
+		sub $t0, $t0, 1
+		syscall
+		addi $a0, $a0, 2
+		addi $a1, $a1, 100
+		bne $t0, $0, loop_victory
+.end_macro
