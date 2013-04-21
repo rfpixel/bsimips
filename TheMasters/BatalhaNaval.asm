b5fbc38d112dc68d0c92a0e1a0dacce37c0366e8
marcelo8g <marcelo8g@gmail.com>
2013-04-21 18:36:09 +0000

    substituição de labels por sub-rotinas. Criação de rotinas para entrada e saida de dados

--- TheMasters/BatalhaNaval.asm
+++ TheMasters/BatalhaNaval.asm
@@ -22,13 +22,16 @@
 
 # declaracoes globais
 .data
-		acertos: 0		# quantidade de acertos do usuario
-		chances_restantes: 5 	# quantidade de chances que o usuario tem
+		acertos: .word 0		# quantidade de acertos do usuario
+		chances_restantes: .word 5 	# quantidade de chances que o usuario tem
+		sim: .word 1			# resposta logica para afirmacao	
+		nao: .word 0			# resposta logica para negacao
+		
 		
 .text
 
 # declaração de procedimentos
-# procedimento criado para exibir mensagens		
+# exibe mensagens
 .macro	ExibeMensagem(%texto)
 	.data
 		mensagemTexto: .asciiz %texto
@@ -39,23 +42,48 @@
 		
 .end_macro 
 
+# exibe caracteres
+.macro	ExibeCaracter($caracter)	
+		move $a0, $caracter		
+		li $v0, 11
+		syscall		
+.end_macro 
+
+
 # Le um caracter alfanumerico
 .macro LerCaracter($valorCaracter)
+	li $v0, 12
+	syscall
+	move $valorCaracter, $v0
 .end_macro 		
 
 # Le um caracter numerico
 .macro LerInteiro($valorInteiro)
+	li $v0, 5
+	syscall
+	move $valorInteiro, $v0
 .end_macro 	
 
-# Le uma coordenada do quadro naval
-.macro LerCoordenada($coordX,$coordY)	
+# Le uma coordenada do Quadro Naval
+.macro LerCoordenada($coordX,$coordY)
+       ExibeMensagem("Informe as coordenadas do alvo linha(0:9), coluna(a:j) :\n\n")
+       ExibeMensagem("linha:\n")
+       LerInteiro($coordX)
+       ExibeMensagem("\ncoluna:\n")
+       LerCaracter($coordY)		
 .end_macro 
 
 # Le um caracter alfanumerico
 .macro contabilizaAcerto($coordX, $coordY, $acerto, $chances)
 .end_macro 				
-	
 
+# Inicializa o Quadro Naval
+.macro inicializaQuadroNaval($elemento)
+.end_macro 
+
+# Fixa valores definidos pelo usuario no Quadro Naval
+.macro FixarNavios($elemento)
+.end_macro
 
 # declaração de mensagens 
 
@@ -64,6 +92,7 @@
 	# declaracao de variaveis
 	
 	# Quadro da Batalha Naval vetores de caracteres
+	linha0: .word ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '
 	linha1: .word ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '
 	linha2: .word ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' 
 	linha3: .word ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '
@@ -73,15 +102,7 @@
 	linha7: .word ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '
 	linha8: .word ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '
 	linha9: .word ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '
-	linha0: .word ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '
-	
-	caracter: .asciiz " "
-	espacamento: .asciiz "\t"
-	quebraLinha: .asciiz "\n"
-	
-	# strings de mensagem
-	tituloJogo: .asciiz ""
-	avisoFimDeJogo: .asciiz "\n\nFim de jogo!\n\n\n"							
+								
 	
 .text # a palavra reservada .text permite armazenar cadeia de caracteres em variaveis	
 	
@@ -92,33 +113,24 @@
 	
 	# declaracao de sub rotinas
 		
-	TelaInicial:	# tela inicial do jogo
-	
-		la $a0, tituloJogo
+	TelaInicial:	# tela inicial do jogo	
+		
 		ExibeMensagem("Batalha Naval\t-\tVersao 1.0 \n\n")
 		#jal ExibeMensagem		# salta para a sub rotina "ExibeMensagem"
 		
-		Loop:
-		
+		Loop:		
 			
-		        #sw $s1, ($s2)		# escreve um caracter na memoria		            
-			lw $a0, linha1($t1)		# recebe o conteudo do vetor
-			jal ExibeCaracter						
+		        #sw $s1, ($s2)			# escreve um caracter na memoria		            
+		        lw $a1, linha1($t1)
+			ExibeCaracter($a1)	# recebe e imprime o conteudo do vetor			
 			addi $t1, $t1, 	4	# incrementa para a proxima posicao			
 			subi $t2, $t2 1		# decrementa o contador de controle do loop			
-			
 									
-			bgtz $t2, Loop
-		
-		la $a0, avisoFimDeJogo	# armazena a o título do jogo no registrador $a0		
-		
-		jal FimJogo		# salta para a sub rotina "FimJogo"
+			bgtz $t2, Loop		# realiza o loop ate que o contador zere		
 		
 		
-	ExibeCaracter:		# exibe caracteres
-		li $v0, 11
-		syscall
-		jr $ra	
+		ExibeMensagem("\n\nFim de jogo!\n\n\n")		
 		
+		jal FimJogo		# salta para a sub rotina "FimJogo"	
 		
-	FimJogo:		# fim do jogo	
+	FimJogo:		# fim do jogo
